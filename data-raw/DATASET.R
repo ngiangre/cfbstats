27a7b374606406759ee0fc158ad8e072ddc7f2d3
Nicholas Giangreco <nick.giangreco@gmail.com>
2026-08-16 20:23:15 -0400

    WIP extract data

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,20 +1,173 @@
-resp_picks <- httr2::request(
+# Save NFL draft pick data
+httr2::request(
   "https://api.collegefootballdata.com/draft/picks"
 ) |>
   httr2::req_headers(
     Authorization = paste("Bearer", Sys.getenv("CFBD_API_KEY"))
   ) |>
-  httr2::req_perform()
-
-picks <-
-  httr2::resp_body_json(resp_picks, simplifyVector = TRUE) |>
+  httr2::req_perform() |>
+  httr2::resp_body_json(
+    simplifyVector = TRUE
+  ) |>
   tibble::tibble() |>
   tidyr::unnest(
     cols = hometownInfo,
     names_sep = "_"
-  )
-
-picks |>
+  ) |>
   arrow::write_parquet(
     "data/picks.parquet"
   )
+
+years <-
+  seq(2010, 2025, 1) |>
+  as.integer()
+
+# Save player statistics from each season
+purrr::walk(
+  years,
+  ~ {
+    tryCatch(
+      {
+        httr2::request(
+          "https://api.collegefootballdata.com/stats/player/season"
+        ) |>
+          httr2::req_url_query(
+            year = .x
+          ) |>
+          httr2::req_headers(
+            Authorization = paste("Bearer", Sys.getenv("CFBD_API_KEY"))
+          ) |>
+          httr2::req_perform() |>
+          httr2::resp_body_json(simplifyVector = TRUE) |>
+          tibble::tibble() |>
+          arrow::write_parquet(
+            paste0("data/player_", .x, "_stats.parquet")
+          )
+      },
+      finally = function(e) e$message
+    )
+  }
+)
+
+# Coaches data
+httr2::request(
+  "https://api.collegefootballdata.com/coaches"
+) |>
+  httr2::req_headers(
+    Authorization = paste("Bearer", Sys.getenv("CFBD_API_KEY"))
+  ) |>
+  httr2::req_perform() |>
+  httr2::resp_body_json(
+    simplifyVector = TRUE
+  ) |>
+  tibble::tibble() |>
+  tidyr::unnest(
+    cols = seasons,
+    names_sep = "_"
+  ) |>
+  arrow::write_parquet(
+    "data/coaches.parquet"
+  )
+
+players <-
+  purrr::map(
+    years,
+    ~ {
+      arrow::read_parquet(
+        paste0("data/player_", .x, "_stats.parquet"),
+        as_data_frame = FALSE
+      ) |>
+        dplyr::pull(
+          player,
+          as_vector = TRUE
+        ) |>
+        unique()
+    }
+  ) |>
+  purrr::flatten_chr() |>
+  unique()
+
+picked <-
+  arrow::read_parquet(
+    "data/picks.parquet",
+    as_data_frame = FALSE
+  ) |>
+  dplyr::pull(
+    name,
+    as_vector = TRUE
+  ) |>
+  unique()
+
+intersect(
+  players,
+  picked
+) |>
+  dplyr::n_distinct()
+
+teams <-
+  purrr::map(
+    years,
+    ~ {
+      arrow::read_parquet(
+        paste0("data/player_", .x, "_stats.parquet"),
+        as_data_frame = FALSE
+      ) |>
+        dplyr::pull(
+          team,
+          as_vector = TRUE
+        ) |>
+        unique()
+    }
+  ) |>
+  purrr::flatten_chr() |>
+  unique() |>
+  sort()
+
+teams_of_picked <-
+  arrow::read_parquet(
+    "data/picks.parquet",
+    as_data_frame = FALSE
+  ) |>
+  dplyr::pull(
+    collegeTeam,
+    as_vector = TRUE
+  ) |>
+  unique() |>
+  sort()
+
+coaches <-
+  arrow::read_parquet(
+    "data/coaches.parquet",
+    as_data_frame = FALSE
+  ) |>
+  dplyr::distinct(
+    firstName,
+    lastName,
+    seasons_school,
+    seasons_year
+  ) |>
+  dplyr::collect()
+
+teams_of_coaches <-
+  arrow::read_parquet(
+    "data/coaches.parquet",
+    as_data_frame = FALSE
+  ) |>
+  dplyr::pull(
+    seasons_school,
+    as_vector = TRUE
+  ) |>
+  unique() |>
+  sort()
+
+intersect(
+  teams_of_picked,
+  teams
+) |>
+  dplyr::n_distinct()
+
+intersect(
+  teams_of_coaches,
+  teams
+) |>
+  dplyr::n_distinct()
